71f28830590d477cddf6bffe8f8d0ad7217f2d9c
Jonathon Anderson <jonathon.anderson@colorado.edu>
2018-06-19 16:35:19 -0600

    Rudimentary namespace support

--- slk.py
+++ slk.py
@@ -18,10 +18,13 @@ def main ():
     subparsers = argparser.add_subparsers(title='subcommands', dest='subcommand')
 
     version = subparsers.add_parser('version')
-    version.set_defaults(func=get_version)
 
     exports = subparsers.add_parser('exports')
-    exports.set_defaults(func=get_exports)
+
+    namespaces = subparsers.add_parser('namespaces')
+    namespaces.add_argument('ids', nargs='*')
+    namespaces.add_argument('--detail', action='store_true', default=False)
+    namespaces.add_argument('--json', action='store_true', default=False)
 
     config = ConfigParser.SafeConfigParser()
     config.read([os.path.expanduser('~/.slk-api.cfg')])
@@ -42,13 +45,27 @@ def main ():
         session_key = None
 
     if args.subcommand == 'version':
-        if args.func:
-            args.func(
+        get_version(
+            url=config.get('api', 'url'),
+            verify=config.getboolean('api', 'verify'),
+        )
+    elif args.subcommand == 'namespaces':
+        if not args.ids:
+            get_namespaces(
+                session_key = session_key,
                 url=config.get('api', 'url'),
                 verify=config.getboolean('api', 'verify'),
             )
-    elif args.func:
-        args.func(
+        for id_ in args.ids:
+            for record in get_namespace(
+                    session_key = session_key,
+                    id_ = id_,
+                    url=config.get('api', 'url'),
+                    verify=config.getboolean('api', 'verify'),
+            ):
+                print_namespace(record, detail=args.detail, json=args.json)
+    elif args.subcommand == 'exports':
+        get_exports(
             session_key = session_key,
             url=config.get('api', 'url'),
             verify=config.getboolean('api', 'verify'),
@@ -75,8 +92,38 @@ def authenticate (url, domain, username, password, verify=True):
 def get_exports (url, session_key, verify=True):
     response = requests.get('{url}/v1/exports'.format(url=url), verify=verify, headers={'X-SDS-SessionKey': session_key})
     for record in response.json()['records']:
-        print(record['_id'], record['attributes']['mountType'], record['attributes']['mountOptions'], record['attributes']['mountHosts'], record['name'], record['nsID'])
+        namespace = list(get_namespace(url, session_key, record['nsID'], verify=verify))[0]
+        print(record['_id'], record['attributes']['mountType'], record['attributes']['mountOptions'], record['attributes']['mountHosts'], record['name'], namespace['path'])
 
+
+def get_namespaces (url, session_key, verify=True, parent_id="1", names=['pl'], attributes={}):
+    response = requests.get('{url}/v1/namespaces'.format(url=url),
+                            verify=verify,
+                            headers={'X-SDS-SessionKey': session_key},
+                            json={'namespaces':{
+                                'parent_id':parent_id,
+                                'names': names,
+                                'attributes': attributes,
+                            }})
+    print(response.json())
+    for record in response.json()['records']:
+        print(record)
+
+
+def get_namespace (url, session_key, id_, verify=True):
+    response = requests.get('{url}/v1/namespaces/{id_}'.format(url=url, id_=id_), verify=verify, headers={'X-SDS-SessionKey': session_key})
+    for record in response.json()['records']:
+        yield record
+
+
+def print_namespace (record, detail=False, json=False):
+    if json:
+        print(record)
+    elif detail:
+        print(record['path'])
+        print('    parent: {}'.format(record['parent_id']))
+    else:
+        print(record['path'])
 
 if __name__ == '__main__':
     main()
