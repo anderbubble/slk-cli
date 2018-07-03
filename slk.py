639ab67b3688e559caf3697505a54b93e5c60c65
Jonathon Anderson <jonathon.anderson@colorado.edu>
2018-07-03 14:54:47 -0600

    Catch-up and adding --delete

--- slk.py
+++ slk.py
@@ -9,6 +9,7 @@ import ConfigParser
 import os
 import pprint
 import requests
+import requests.status_codes
 import urllib3
 
 
@@ -40,11 +41,12 @@ def main ():
             url = config.get('api', 'url'),
             verify = config.getboolean('api', 'verify'),
         ))
+    elif args.delete:
+        response = delete_dynamic(args.path, **kwargs)
+        print(response.status_code, requests.status_codes._codes[response.status_code][0])
     else:
-        record_type = get_record_type_from_path(args.path)
-
         for record in get_records_dynamic(args.path, **kwargs):
-            print_record(record, record_type, dsv=args.dsv, pprint_=args.pprint)
+            print_record(record, path=args.path, dsv=args.dsv, pprint_=args.pprint)
 
 
 def get_argparser ():
@@ -52,6 +54,7 @@ def get_argparser ():
     argparser.add_argument('--authenticate', action='store_true', default=False)
     argparser.add_argument('--pprint', action='store_true', default=False)
     argparser.add_argument('--dsv', action='store_true', default=False)
+    argparser.add_argument('--delete', action='store_true', default=False)
     argparser.add_argument('path')
     return argparser
 
@@ -88,9 +91,17 @@ def get_records_dynamic (path, url, version, session_key=None, verify=True):
     return response.json()['records']
 
 
+def delete_dynamic (path, url, version, session_key=None, verify=True):
+    headers = {}
+    if session_key is not None:
+        headers['X-SDS-SessionKey'] = session_key
+    response = requests.delete('/'.join((url, version, path)), verify=verify, headers=headers)
+    return response
+
+
 def check_errors (response_dict):
     if 'errors' in response_dict:
-        raise RESTErrors(*response_dict['errors'])
+        raise RESTErrors(response_dict)
 
 
 def get_namespaces (url, model, session_key=None, verify=True, parent_id="1", names=['pl'], attributes={}):
@@ -112,7 +123,11 @@ SIMPLE_FIELDS = {
     'pools': ('_id', 'name', 'description'),
     'namespaces': ('_id', 'posix_uid', 'posix_gid', 'posix_mode', 'path'),
     'exports': ('_id', 'name', 'description', 'nsID'),
+    'users': ('_id', 'name'),
+    'roles': ('description', ),
 }
+SIMPLE_FIELDS['special'] = SIMPLE_FIELDS['users']
+SIMPLE_FIELDS['whoami'] = SIMPLE_FIELDS['users']
 
 
 def get_record_type_from_path (path):
@@ -123,9 +138,12 @@ def get_record_type_from_path (path):
         raise NotImplementedError(path)
 
 
-def print_record (record, type_, dsv=False, pprint_=False):
+def print_record (record, path=None, dsv=False, pprint_=False):
     if dsv:
-        print(simple_dsv(record, type_))
+        if path is None:
+            raise TypeParseError('unable to determine record type without path')
+        record_type = get_record_type_from_path(path)
+        print(simple_dsv(record, record_type))
     elif pprint_:
         pprint.pprint(record)
     else:
@@ -141,6 +159,9 @@ def simple_dsv (record, type_):
 
 
 class RESTErrors (Exception): pass
+
+
+class TypeParseError (Exception): pass
 
 
 if __name__ == '__main__':
